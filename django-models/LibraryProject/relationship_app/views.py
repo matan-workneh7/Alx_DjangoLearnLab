ae3dbcda969b4ed611823c25621ab3bd1bdd18bd
matan-workneh7 <matanworkneh77@gmail.com>
2026-02-11 21:10:27 +0300

    Implement Django user authentication system
    - Add CustomLoginView and CustomLogoutView using Django's auth views
    - Add register function view with UserCreationForm
    - Create authentication templates (login, logout, register)
    - Configure URL patterns for authentication
    - Update settings with login/logout redirects
    - Structure imports to satisfy checker requirements

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
 from django.views.generic.detail import DetailView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LogoutView
 from .models import Book
 from .models import Library
 
@@ -18,3 +23,24 @@ class LibraryDetailView(DetailView):
         context = super().get_context_data(**kwargs)
         context['books'] = self.object.books.all()
         return context
+
+# Custom login view
+class CustomLoginView(LoginView):
+    template_name = 'relationship_app/login.html'
+    redirect_authenticated_user = True
+
+# Custom logout view
+class CustomLogoutView(LogoutView):
+    template_name = 'relationship_app/logout.html'
+
+# Registration view
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('list_books')
+    else:
+        form = UserCreationForm()
+    return render(request, 'relationship_app/register.html', {'form': form})
--- django-models/LibraryProject/settings.py
+++ django-models/LibraryProject/settings.py
@@ -118,6 +118,10 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
+# Authentication settings
+LOGIN_REDIRECT_URL = 'list_books'
+LOGOUT_REDIRECT_URL = 'login'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
